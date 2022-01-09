236328ed6b6e8888b7dd9a17716b52ace3807da1
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-09 21:23:43 +0100

    adde routes for cities.

--- src/routes/cities/index.js
+++ src/routes/cities/index.js
@@ -26,5 +26,52 @@ citiesRouter
       next(e);
     }
   });
+citiesRouter
+  .route("/:cityId")
+  .get(async (req, res, next) => {
+    try {
+      if (req.params.cityId.length !== 36)
+        return next({ code: 400, msg: "invalid id" });
+      const user = await Cities.findByPk(req.params.cityId);
+      res.send(user);
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  })
+  .put(async (req, res, next) => {
+    console.log(req.params);
+    try {
+      if (req.params.cityId.length !== 36)
+        return res.status(400).send("Invalid ID");
+      if (req.body === {}) return res.status(400).send("Must Provide A Body");
+      console.log("Req.Body", req.body);
+      const editUser = await Cities.update(req.body, {
+        where: { id: req.params.cityId },
+        returning: true,
+      });
+      res.status(201).send(editUser[1][0]);
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      if (req.params.cityId.length !== 36)
+        return res.status(400).send("Invalid ID");
+      const deleteUser = await Cities.destroy({
+        where: { id: req.params.cityId },
+      });
+      if (deleteUser > 0) {
+        res.send("ok");
+      } else {
+        res.status(404).send("Not found");
+      }
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  });
 
 export default citiesRouter;
