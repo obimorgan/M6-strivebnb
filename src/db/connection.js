d72a803f531781711c7a865f5e062fc70e9a8073
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-09 10:09:48 +0100

    errohandler folder

---
+++ src/db/connection.js
@@ -0,0 +1,32 @@
+/** @format */
+
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+const { DATABASE_URL } = process.env;
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+export const testDB = async () => {
+  try {
+    await sequelize.authenticate({ logging: false });
+    console.log("DB is authenticated");
+    await sequelize
+      .sync()
+      .then(() => console.log("The server is connected to postgres"));
+    console.log("DB is established");
+  } catch (error) {
+    console.log("Failed to authenticate", error);
+  }
+};
+
+export default sequelize;
